a880867a9ba639b3f95e16009cb59ab189aed619
Hyejin Shin <enthusiastic.shj@gmail.com>
2017-12-27 22:22:23 +0900

    omok-game basic asset

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.col').forEach(el => {
+        el.addEventListener('click', e => {
+            console.log(Array.from(el.classList));
+            console.log(Array.from(el.closest('.row').classList));
+        });
+    });
+});
